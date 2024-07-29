4dad25310b50f583c02cd83d90d5b966ad995527
Nitesh Kumar <nitesh098suthar@gmail.com>
2024-07-29 15:53:39 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import './style.css'
+
+const inputText = document.getElementsByName("inputText")[0] as HTMLInputElement;
+const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
+
+const allTask = document.getElementsByClassName("allTask")[0] as HTMLDivElement;
+
+
+const creatingElements = () => {
+  const oneTask = document.createElement("div")
+  oneTask.classList.add("one-task")
+  
+  const checkBox = document.createElement("input")
+  checkBox.type = "checkbox"
+  checkBox.id = "checkBox" 
+  oneTask.append(checkBox)
+  
+  const todoText = document.createElement("p")
+  todoText.classList.add("todo-task")
+  oneTask.append(todoText)
+  todoText.innerHTML =  inputText.value   
+  
+  const deleteButton = document.createElement("button")
+  deleteButton.innerHTML = "Del"
+  deleteButton.classList.add("deleteButton") 
+  oneTask.append(deleteButton)
+
+  allTask.append(oneTask)
+}
+
+submitButton.addEventListener("click", ()=>{
+ creatingElements() 
+ inputText.value = "" 
+ 
+})
+
+//getting all deleteButtons
+
+
+
